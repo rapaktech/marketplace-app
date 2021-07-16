dd8a26bb47919a1af86402e22717880bdb964895
O-TECH1 <83675127+O-TECH1@users.noreply.github.com>
2021-07-16 13:43:50 +0100

    Update db-conn.php

--- db-conn.php
+++ db-conn.php
@@ -1,35 +1,48 @@
 <?php
-    /* Modify $username, $password and $dbname variable to match your own */
-    
-    $servername = "localhost";
-    $username = "username";
-    $password = "password";
-    $dbname = "Marketplace";
-    $conn = new mysqli ($servername, $username, $password, $dbname);
+/* Modify $username, $password and $dbname variable to match your own */
 
-    if ($conn->connect_error) {
-        $conn = new mysqli ($servername, $username, $password, $dbname);
-    }
+$servername = "localhost";
+$username = "root";
+$pass = "";
+$dbname = "marketplace";
+define('servername', 'localhost');
+define('username', 'root');
+define('pass', '');
+define('dbname', 'marketplace');
+$conn = new mysqli (servername, username, pass, dbname);
 
-    $createUser = $conn->prepare("INSERT INTO Users (user_firstname, user_lastname, user_email, user_password) 
+
+$createUser = $conn->prepare("INSERT INTO Users (user_firstname, user_lastname, user_email, user_password) 
     VALUES (?, ?, ?, ?)");
-    $createUser->bind_param("ssss", $firstName, $lastName, $email, $hashedPassword);
+$createUser->bind_param("ssss", $firstName, $lastName, $email, $hashedPassword);
 
-    $findUser = $conn->prepare("SELECT user_num, user_firstname, user_password FROM Users WHERE user_email=?");
-    $findUser->bind_param("s", $email);
+$findUser = $conn->prepare("SELECT user_num, user_firstname, user_password FROM Users WHERE user_email=?");
+$findUser->bind_param("s", $email);
 
-    $resetPassword = $conn->prepare("UPDATE Users SET user_password=? WHERE user_num=?");
-    $resetPassword->bind_param("si", $hashedPassword, $userId);
+function resetPass($hashed,$uId) {
+    $conn = new mysqli (servername, username, pass, dbname);
+    $res = false;
+    try {
+        $resetPassword = $conn->prepare("UPDATE Users SET user_password=? WHERE user_num=?");
+        $resetPassword->bind_param("si", $hashed, $uId);
+        $resetPassword->execute();
+        $res = true;
+    } catch (PDOException $e) {
+        $res = $e->getMessage();
+    }
+    return $res;
+}  
 
-    $createItem = $conn->prepare("INSERT INTO Items (item_name, item_description, item_price, item_creator_email) VALUES (?, ?, ?, ?)");
-    $createItem->bind_param("ssss", $itemName, $itemDescription, $itemPrice, $email);
 
-    $findItems = $conn->prepare("SELECT item_id, item_name, item_description, item_price FROM Items WHERE item_creator_email=?");
-    $findItems->bind_param("s", $email);
+$createItem = $conn->prepare("INSERT INTO Items (item_name, item_description, item_price, item_creator_email) VALUES (?, ?, ?, ?)");
+$createItem->bind_param("ssss", $itemName, $itemDescription, $itemPrice, $email);
 
-    $updateItem = $conn->prepare("UPDATE Items SET item_name=?, item_description=?, item_price=? WHERE item_id=?");
-    $updateItem->bind_param("sssi", $updatedItemName, $updatedItemDescription, $updatedItemPrice, $itemId);
+$findItems = $conn->prepare("SELECT item_id, item_name, item_description, item_price FROM Items WHERE item_creator_email=?");
+$findItems->bind_param("s", $email);
 
-    $deleteItem = $conn->prepare("DELETE FROM Items WHERE item_id=?");
-    $deleteItem->bind_param("i", $itemId);
+$updateItem = $conn->prepare("UPDATE Items SET item_name=?, item_description=?, item_price=? WHERE item_id=?");
+$updateItem->bind_param("sssi", $updatedItemName, $updatedItemDescription, $updatedItemPrice, $itemId);
+
+$deleteItem = $conn->prepare("DELETE FROM Items WHERE item_id=?");
+$deleteItem->bind_param("i", $itemId);
 ?>
